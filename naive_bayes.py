bc00dd285e2a08c8862e80f3516fd698b62217da
Devendra Sawod <dsawod420@unm.edu>
2022-03-18 10:46:20 -0600

    update

--- naive_bayes.py
+++ naive_bayes.py
@@ -8,17 +8,19 @@ def main():
     # VOCAB_SIZE = _getWordsCountInDictionary()
 
     csr_rows = []
-    chunksize = 10 ** 3
+    chunksize = 3
     s_time_dask = time.time()
-    with pd.read_csv("training.csv", chunksize=chunksize) as reader:
+    with pd.read_csv("trainingpoems.csv", chunksize=chunksize, header=None) as reader:
         for chunk in reader:
             chunk_csrs = _processChunk(chunk)
-            csr_rows.append(chunk_csrs)
+            csr_rows = csr_rows + chunk_csrs
 
     e_time_dask = time.time()
     print("Read time 1000: ", (e_time_dask - s_time_dask), "seconds")
 
-    print(len(csr_rows))
+    print("length of CSR matrices list", len(csr_rows))
+    array0 = csr_rows[0].toarray()
+    print(array0)
 
 
 main()
