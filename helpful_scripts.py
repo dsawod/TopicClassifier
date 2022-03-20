70ce8a00328d112c17673464bf34dd668ede9874
maoki109 <mariaoki109@gmail.com>
2022-03-19 19:14:50 -0600

    getCountsPerClass function

--- helpful_scripts.py
+++ helpful_scripts.py
@@ -1,7 +1,7 @@
 from scipy.sparse import csr_matrix
 import numpy
 
-# This method processes the chucnk of training file read
+# This method processes the chunk of training file read
 # It returns list of compressed matrices of each row in the chunk.
 def _processChunk(chunk_df):
     size = len(chunk_df)
@@ -9,6 +9,7 @@ def _processChunk(chunk_df):
     for i in range(size):
         row = chunk_df.iloc[i]  # This is Series
         row_as_list = row.values.tolist()  # Convert series to a list
+        print(row_as_list)
         row_matrix = numpy.array(
             row_as_list
         )  # convert list to a matrix which is sparse
@@ -53,3 +54,32 @@ def _getTotalWordsCount(document):
         count = count + document[i]
 
     return count
+
+"""
+Takes in a Pandas dataframe with first element being the document ID, the last element being the class of the document, 
+and the elements in between are the word counts of the document. 
+Returns a list of lists. Each list contains the word counts for each class.
+"""
+def _getCountsPerClass(df):
+    # Convert Pandas dataframe to list
+    dfList = df.values.tolist()
+    # maxNum stores the largest class number
+    # Note: this uses the class number of the last document, but we are not guaranteed that the last document
+    # will have the highest class number.
+    maxNum = dfList[-1][-1]
+    # [[] for i in range(maxNum)] creates a list of empty lists with the number of classes.
+    # Note: using [[]]*maxNum doesn't work because each list will be pointing to the same one and causes problems later.
+    # totalCounts first holds all the document word counts for each class.
+    # Will later hold total word count for each class.
+    totalCounts = [[] for i in range(maxNum)]
+    # Group documents by class in totalCounts list
+    for i in range(len(dfList)):
+        row = dfList[i]
+        classNum = row[-1]
+        totalCounts[classNum - 1].append(row[1:-2])
+    # For each class in totalCounts, add word count for indices in each document to get total word count.
+    # Sum shortcut found from:
+    # https://stackoverflow.com/questions/64264392/adding-numbers-in-the-same-indices-in-list-of-lists
+    for i in range(len(totalCounts)):
+        currentClass = totalCounts[i]
+        totalCounts[i] = list(map(sum, zip(*currentClass)))
--- naive_bayes.py
+++ naive_bayes.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from helpful_scripts import _getWordsCountInDictionary, _processChunk, _getClassProbability
+from helpful_scripts import _getWordsCountInDictionary, _processChunk, _getClassProbability, _getCountsPerClass
 import time
 
 
@@ -11,25 +11,27 @@ def main():
     class_ls = []
     chunksize = 10 ** 2
     s_time_dask = time.time()
-    with pd.read_csv("trainingpoems.csv", chunksize=chunksize, header=None) as reader:
-        for chunk in reader:
-            last_column = chunk.iloc[:, -1]
-            last_column_ls = last_column.values.tolist()
-            class_ls = class_ls + last_column_ls
-            chunk_csrs = _processChunk(chunk)
-            csr_rows = csr_rows + chunk_csrs
+    # with pd.read_csv("trainingpoems.csv", chunksize=chunksize, header=None) as reader:
+    #     for chunk in reader:
+    #         last_column = chunk.iloc[:, -1]
+    #         last_column_ls = last_column.values.tolist()
+    #         class_ls = class_ls + last_column_ls
+    #         chunk_csrs = _processChunk(chunk)
+    #         csr_rows = csr_rows + chunk_csrs
 
     e_time_dask = time.time()
-    print("Read time 1000: ", (e_time_dask - s_time_dask), "seconds")
+    # print("Read time 1000: ", (e_time_dask - s_time_dask), "seconds")
+    #
+    # print("length of CSR matrices list", len(csr_rows))
+    # array0 = csr_rows[0].toarray()
+    # print(array0)
+    # print(csr_rows[0])
+    # print(class_ls)
+    #
+    # probability_ls = _getClassProbability(class_ls)
+    # print(probability_ls)
 
-    print("length of CSR matrices list", len(csr_rows))
-    array0 = csr_rows[0].toarray()
-    print(array0)
-    print(csr_rows[0])
-    print(class_ls)
-
-    probability_ls = _getClassProbability(class_ls)
-    print(probability_ls)
-
+    df = pd.read_csv("training-poems.csv", header=None)
+    _getCountsPerClass(df)
 
 main()
