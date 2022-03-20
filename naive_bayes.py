c9bfbdfb852ae5ef4dd3e5dd7fe6d417d0880399
Devendra Sawod <dsawod420@unm.edu>
2022-03-20 16:00:03 -0600

    my update

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
-from helpful_scripts import _getWordsCountInDictionary, _processChunk, _getClassProbability
+from helpful_scripts import (
+    _getWordsCountInDictionary,
+    _processChunk,
+    _getClassProbability,
+)
 import time
+from scipy.sparse import csr_matrix
 
 
 def main():
@@ -9,7 +14,8 @@ def main():
 
     csr_rows = []
     class_ls = []
-    chunksize = 10 ** 2
+    chunksize = 3  # optimal chunk size 100
+    ls = []
     s_time_dask = time.time()
     with pd.read_csv("trainingpoems.csv", chunksize=chunksize, header=None) as reader:
         for chunk in reader:
@@ -20,16 +26,7 @@ def main():
             csr_rows = csr_rows + chunk_csrs
 
     e_time_dask = time.time()
-    print("Read time 1000: ", (e_time_dask - s_time_dask), "seconds")
-
-    print("length of CSR matrices list", len(csr_rows))
-    array0 = csr_rows[0].toarray()
-    print(array0)
-    print(csr_rows[0])
-    print(class_ls)
-
-    probability_ls = _getClassProbability(class_ls)
-    print(probability_ls)
+    print("Read time 50: ", (e_time_dask - s_time_dask), "seconds")
 
 
 main()
